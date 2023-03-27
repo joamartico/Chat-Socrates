72814b4458593460df0e019365e03e3933726936
Joa Marticorena <joamartico@gmail.com>
2023-03-27 01:17:46 -0300

    mvp

--- pages/index.js
+++ pages/index.js
@@ -4,52 +4,9 @@ import styled from "styled-components";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const API_URL = "https://api.openai.com/v1/chat/completions";
 
-const messages = {
-	forChild: [
-		{
-			role: "system",
-			content: "explicame como si fuese un niño de 10 años",
-		},
-		{ role: "user", content: "Qué comen las plantas" },
-		{
-			role: "assistant",
-			content: "Las plantas no necesitan comer, usan la energía del sol",
-		},
-	],
-	simple: [
-		{
-			role: "system",
-			content: "answers questions in a simple way",
-		},
-		{ role: "user", content: "Qué comen las plantas" },
-		{
-			role: "assistant",
-			content:
-				"Las plantas producen su propio alimento a través de la fotosíntesis. Utilizan la energía del sol para convertir el dióxido de carbono (CO2) y el agua (H2O) en glucosa (un tipo de azúcar) y oxígeno (O2).",
-		},
-	],
-	complex: [
-		{
-			role: "system",
-			content: "answer questions as accurately as possible",
-		},
-		{ role: "user", content: "Qué comen las plantas" },
-		{
-			role: "assistant",
-			content:
-				"Las plantas obtienen la mayor parte de su alimento a través de un proceso llamado fotosíntesis. Durante la fotosíntesis, las plantas utilizan la energía de la luz solar para convertir el dióxido de carbono (CO2) y el agua (H2O) en azúcares simples, como la glucosa, y liberan oxígeno (O2) como subproducto. Este proceso ocurre en las células de la hoja de la planta, donde se encuentran los cloroplastos, que son los orgánulos celulares que contienen clorofila, la molécula que absorbe la luz solar. La luz solar se convierte en energía química que se utiliza para impulsar la fotosíntesis. Además de la fotosíntesis, las plantas también pueden obtener nutrientes del suelo a través de las raíces. Las raíces absorben agua y nutrientes del suelo, incluyendo minerales como nitrógeno, fósforo y potasio, que son necesarios para el crecimiento y la reproducción de las plantas.",
-		},
-	],
-};
-
 export default function Home() {
 	const [promptValue, setPromptValue] = useState("");
-	const [responses, setResponses] = useState({
-		forChild: "",
-		simple: "",
-		complex: "",
-	});
-	const [activeTab, setActiveTab] = useState("forChild");
+	const [messages, setMessages] = useState([]);
 
 	const handleChange = (event) => {
 		setPromptValue(event.target.value);
@@ -58,6 +15,18 @@ export default function Home() {
 	};
 
 	async function askToGpt() {
+		setPromptValue("");
+		const newMessages = [
+			{
+				role: "system",
+				content:
+					"You are socrates. Do not give answers, make the user find the answer to his question by asking him deeper questions.",
+			},
+			...messages,
+			{ role: "user", content: promptValue },
+		];
+		setMessages(newMessages);
+
 		const response = await fetch(API_URL, {
 			method: "POST",
 			headers: {
@@ -66,163 +35,56 @@ export default function Home() {
 			},
 			body: JSON.stringify({
 				model: "gpt-3.5-turbo",
-				messages: [...messages[activeTab], { role: "user", content: promptValue }],
-				stream: true,
-				temperature: 0.4,
+				messages: newMessages,
+				// stream: true,
+				temperature: 0.6,
 				stop: ["\ninfo:"],
-				// max_tokens: activeTab != "complex" && 200,
 			}),
 		});
 
-		const reader = response.body.getReader();
-		const decoder = new TextDecoder("utf-8");
-
-		while (true) {
-			const { value, done } = await reader.read();
-
-			if (done) {
-				setResponses((prev) => {
-					const newResponses = { ...prev };
-					newResponses[activeTab] =
-						newResponses[activeTab].concat("<br/><br/>");
-					return newResponses;
-				});
-				break;
-			}
-
-			const chunk = await decoder.decode(value);
-
-			const data = chunk
-				.split("\n")
-				// .filter(Boolean)
-				.map((line) => line.replace("data: ", ""));
-
-			data.forEach((line) => {
-				try {
-					const json = JSON.parse(line);
-					console.log("json: ", json);
-					const token = json.choices[0]?.delta?.content;
-
-					token &&
-						setResponses((prev) => {
-							const newResponses = { ...prev };
-							newResponses[activeTab] =
-								newResponses[activeTab].concat(token);
-							return newResponses;
-						});
-				} catch (err) {
-					console.log(err);
-				}
-			});
-
-			// setResponses(prev => prev.concat(chunk))
-		}
-
-		// const data = await response.json();
-		// console.log(data);
-		// setResponses(data.choices[0].message.content);
+		const data = await response.json();
+		console.log(data);
+		const newMessages2 = [...newMessages, data.choices[0].message];
+		setMessages(newMessages2);
 	}
 
 	return (
 		<>
-
 			<ion-content fullscreen>
-
-				<TabBar>
-					<TabButton
-						active={activeTab === "forChild"}
-						onClick={() => setActiveTab("forChild")}
-						background="linear-gradient(to right, #1be361, #c1f13b)"
-					>
-						For Child
-					</TabButton>
-					<TabButton
-						active={activeTab === "simple"}
-						onClick={() => setActiveTab("simple")}
-						background="linear-gradient(to right, #ffeb3b, #ffc107)"
-					>
-						Simple
-					</TabButton>
-					<TabButton
-						active={activeTab === "complex"}
-						onClick={() => setActiveTab("complex")}
-						background="linear-gradient(to right, #ffc107, #ff5722)"
-					>
-						Complex
-					</TabButton>
-				</TabBar>
-
 				<Container>
-					<GradientTitle>Ask me anything</GradientTitle>
+					<GradientTitle>Talk with Sócrates</GradientTitle>
 
-					<TextArea
-						value={promptValue}
-						onChange={handleChange}
-						placeholder="Type something..."
-					/>
+					<Scroll>
+						{messages?.map(
+							(message) =>
+								message.content &&
+								message.role != "system" && (
+									<Message role={message.role}>
+										{message.content}
+									</Message>
+								)
+						)}
+					</Scroll>
 
-					<Button onClick={askToGpt}>Ask</Button>
-
-					<p
-						dangerouslySetInnerHTML={{
-							__html: responses[activeTab],
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							askToGpt();
 						}}
-					/>
+					>
+						<TextArea
+							value={promptValue}
+							onChange={handleChange}
+							placeholder="Type something..."
+						/>
+
+						<Button>Ask</Button>
+					</form>
 				</Container>
 			</ion-content>
 		</>
 	);
 }
-
-const TabBar = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-around;
-	align-items: center;
-	height: 54px;
-	border-radius: 27px;
-	position: relative;
-	background: white;
-	width: 90vw;
-	max-width: 700px;
-	margin: auto;
-	margin-top: 20px;
-	margin-bottom: 100px;
-
-	&::after {
-		content: "";
-		background: linear-gradient(to right, #0072ff, #00c6ff);
-		background: #dadada;
-		width: 100%;
-		height: 100%;
-		padding: 3px;
-		border-radius: 33px;
-		position: absolute;
-		z-index: -1;
-		/* opacity: 0.4; */
-	}
-`;
-
-const TabButton = styled.button`
-	background-color: transparent;
-	color: #999;
-	border: none;
-	font-size: 16px;
-	font-weight: bold;
-	font-family: "Montserrat", sans-serif;
-	cursor: pointer;
-	padding: 12px 16px;
-	&:focus {
-		outline: none;
-	}
-	${(props) =>
-		props.active &&
-		`
-      background: ${props.background};
-      color: #fff;
-      border-radius: 20px;
-      `}
-`;
 
 const Container = styled.div`
 	margin: auto;
@@ -238,6 +100,14 @@ const Container = styled.div`
 	line-height: 30px;
 `;
 
+const Scroll = styled.div`
+	height: 100%;
+	max-height: 95vh;
+	width: 100%;
+	overflow-y: scroll;
+	margin-bottom: 30px;
+`;
+
 const GradientTitle = styled.h1`
 	font-size: 2rem;
 	font-family: "Montserrat", "Open Sans", sans-serif;
@@ -250,9 +120,20 @@ const GradientTitle = styled.h1`
 	/* margin-top: -100px; */
 `;
 
-const TextArea = styled.textarea`
+const Message = styled.div`
 	width: 100%;
-	min-height: 125px;
+	background: #cef;
+	border-radius: 4px;
+	padding: 5px 10px;
+	margin-bottom: 15px;
+	background: ${(props) => (props.role == "user" ? "#cef" : "#ddd")};
+`;
+
+
+const TextArea = styled.input`
+	width: 100%;
+	min-height: fit-content;
+	/* height: ; */
 	padding: 10px;
 	font-size: 16px;
 	border: none;
@@ -263,10 +144,12 @@ const TextArea = styled.textarea`
 		outline: none;
 	}
 	margin-bottom: 15px;
+	margin-top: auto;
 `;
 
 const Button = styled.button`
 	background-color: #0072ff;
+	width: 100%;
 	color: #fff;
 	padding: 16px 24px;
 	border: none;
@@ -280,5 +163,5 @@ const Button = styled.button`
 	font-size: 16px;
 	font-weight: bold;
 	font-family: "Montserrat", "Open Sans", sans-serif;
-	margin-bottom: 40px;
+	margin-bottom: 30px;
 `;
